763f65016879cece04f9b128be4fd84f832c6401
ggang89 <ggang89@gmail.com>
2024-09-17 15:28:55 +0900

    레이아웃 파일만들고, createBrowserRouter로 렌더링

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
-createRoot(document.getElementById('root')!).render(
+import './index.css'
+import { Layout } from './components/layout.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[],
+  },
+])
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
