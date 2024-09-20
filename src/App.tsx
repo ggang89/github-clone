a869ff5aaa980aa1fd137eea7386edc875a5ac75
ggang89 <ggang89@gmail.com>
2024-09-20 22:55:53 +0900

    Snackbar,Alert컴포넌트 추가 => id가 3글자 미만일 때 state로 상태바꿔서 경고창 보이도록

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+import { useState } from "react";
 export default function App() {
+  const [showing, setIsShowing] = useState(false);
   //console.dir(window);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -10,20 +13,30 @@ export default function App() {
     const id = form.elements.namedItem("username") as HTMLInputElement;
     console.log(id.value);
     if (id.value.length < 3) {
-      alert("username은 3글자 이상이어야 합니다.")
-    } 
+      setIsShowing(true);
+      return;
+    }
   };
+  const handleClick = () => {};
   return (
-    <form onSubmit={onSubmit}>
-      <TextField
-        name="username"
-        id="outlined-basic"
-        label="to search username"
-        variant="outlined"
-      />
-      <Button variant="contained" type="submit">
-        버튼
-      </Button>
-    </form>
+    <>
+      {showing && (
+        <Snackbar open={true} autoHideDuration={6000}>
+          <Alert severity="warning">3글자 이상 입력하세요</Alert>
+        </Snackbar>
+      )}
+      <form onSubmit={onSubmit}>
+        <TextField
+          name="username"
+          id="outlined-basic"
+          label="to search username"
+          variant="outlined"
+        />
+
+        <Button variant="contained" type="submit" onClick={handleClick}>
+          버튼
+        </Button>
+      </form>
+    </>
   );
 }
