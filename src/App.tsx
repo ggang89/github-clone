a18583917a659581c4059bfc1edb70692ff9e264
ggang89 <ggang89@gmail.com>
2024-09-20 22:05:11 +0900

    mui에서 textField 컴포넌트 import해서 form event 연결 *button 컴포넌트 type=submit지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import Button from "@mui/material/Button";
-
-
+import TextField from "@mui/material/TextField";
 
 export default function App() {
+  //console.dir(window);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    console.dir(form);
+    const id = form.elements.namedItem("username") as HTMLInputElement;
+    console.log(id.value);
+  };
   return (
-    <div>
-      <Button variant="contained">버튼</Button>
-    </div>
+    <form onSubmit={onSubmit}>
+      <TextField
+        name="username"
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+      />
+      <Button variant="contained" type="submit">
+        버튼
+      </Button>
+    </form>
   );
-  
 }
