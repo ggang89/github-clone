41d82f4f973f919232519dfb530fe712780f1906
ggang89 <ggang89@gmail.com>
2024-09-21 16:50:37 +0900

    글자수에 따라 유효성 검사를 다르게 하도록 조건과 state를 바꿔줌

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ import Alert from "@mui/material/Alert";
 import Snackbar from "@mui/material/Snackbar";
 import { useState } from "react";
 export default function App() {
-  const [showing, setIsShowing] = useState(false);
+  const [showing, setIsShowing] = useState({isOpen: false, text: ""});
   //console.dir(window);
-
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.currentTarget;
     const id = form.elements.namedItem("username") as HTMLInputElement;
     console.log(id.value);
     if (id.value.length < 3) {
-      setIsShowing(true);
+      setIsShowing({isOpen: true, text: "세글자 이상"});
+      return;
+    }
+    if (id.value.length > 10) {
+      setIsShowing({ isOpen: true, text: "10자 이하로 입력하세요" });
       return;
     }
   };
   const handleClick = () => {};
   return (
     <>
-      {showing && (
+      {showing.isOpen && (
         <Snackbar open={true} autoHideDuration={6000}>
-          <Alert severity="warning">3글자 이상 입력하세요</Alert>
+          <Alert severity="warning">{ showing.text}</Alert>
         </Snackbar>
       )}
       <form onSubmit={onSubmit}>
