7ba6d618cf33eea380849ac8e00c182431e2dc64
ggang89 <ggang89@gmail.com>
2024-10-10 05:26:38 +0900

    list로 받은 값 그려줌

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,11 +1,21 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Divider from '@mui/material/Divider';
 import { useStore } from "../use-alert";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Form() {
   const [search, setSearch] = useState("ggang89");
   const [list, setList] = useState([]);
+  console.log(list);
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.currentTarget;
@@ -31,9 +41,10 @@ export default function Form() {
       const json = await response.json();
       //@ts-expect-error 타입지정필요
       const lists = json.items.map((item) => ({
-        login: item.login,
-        url: item.url,
+        loginId: item.login,
+        url: item.html_url,
         imgSrc: item.avatar_url,
+
       }));
       setList(lists);
     };
@@ -62,6 +73,46 @@ export default function Form() {
           버튼
         </Button>
       </form>
+      {/* <Box sx={{ width: "100%" }}>
+        {list.map((user) => (
+          <Stack
+            direction="column"
+            spacing={2}
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Avatar
+              alt={user.loginId}
+              src={user.imgSrc}
+              sx={{ width: 70, height: 70 }}
+            />
+            <p>
+              <Link to={user.url}>{user.loginId}</Link>
+            </p>
+          </Stack>
+        ))}
+      </Box> */}
+      <List sx={{ width: "100%", maxWidth: 360 }}>
+        {list.map((user) => (
+          <>
+            <ListItem alignItems="center">
+              <ListItemAvatar>
+                <Avatar
+                  alt={user.loginId}
+                  src={user.imgSrc}
+                  sx={{ width: 40, height: 40 }}
+                />
+              </ListItemAvatar>
+              <Link to={user.url}>
+                <ListItemText primary={user.loginId} />
+              </Link>
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </>
+        ))}
+      </List>
     </>
   );
 }
